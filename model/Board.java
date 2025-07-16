9a09fc5fb6a2666af528879187b095ffed044d2b
PixelRei <playboi.rei17@gmail.com>
2025-07-16 15:07:26 +0200

    Update Board.java

--- model/Board.java
+++ model/Board.java
@@ -49,8 +49,11 @@ public class Board{
     return false;   
     }
     public boolean isDraw(){
-        for(int i=0; i<3; i++){
-            if(grid[i][0] == ' ' || )
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i][j] == ' ') return false;
+            }
         }
+        return !isWin();
     }
 }
