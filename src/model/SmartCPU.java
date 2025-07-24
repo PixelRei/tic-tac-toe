ffd208d1eb5b9c243974885fb51dfeff5efc1c53
PixelRei <playboi.rei17@gmail.com>
2025-07-24 11:47:17 +0200

    Update SmartCPU.java

--- src/model/SmartCPU.java
+++ src/model/SmartCPU.java
@@ -17,7 +17,7 @@ public class SmartCPU {
         if(board.getCell(2, 2) == ' '){
             board.makeMove(2, 2);
         }else{
-            
+
         }
     }
     public char[][] CheckOptions(){
@@ -25,14 +25,14 @@ public class SmartCPU {
         char[][] option = null;
         for(int j=0; j<3; j++){
             for(int i=0; i<3; i++){
-                if(board.getCell(0, i) == Person){
+                if(board.getCell(j, i) == Person){
                     P++;
                 }else{
                     option[j][i] = board.getCell(j, i);
                 }
-                if(P == 2) break;
-                else P = 0;
             }
+            if(P == 2) break;
+            else P = 0;
         }
         return option;
     }
